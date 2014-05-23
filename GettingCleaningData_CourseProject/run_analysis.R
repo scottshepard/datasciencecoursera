dfc7a80fd0f80fdd372a443d3b33da36bda77b67
scotts <scotts@esparklearning.com>
2014-05-22 23:09:56 -0500

    complete run_analysis script with TidyData output

--- GettingCleaningData_CourseProject/run_analysis.R
+++ GettingCleaningData_CourseProject/run_analysis.R
@@ -1,5 +1,7 @@
 # The is a script for the course project in Getting and Cleaning Data
 
+library(plyr)
+
 # First clear your environnment
 rm(list=ls())
 
@@ -18,16 +20,29 @@ train_activities <- readLines("train/y_train.txt")
 
 varnames <- read.table("features.txt")
 names(test_data) <- names(train_data) <- varnames$V2
-
+# Add subject and activity ids
 test_data$subject_id <- test_subjects
 train_data$subject_id <- train_subjects
 test_data$activity_id <- test_activities
 train_data$activity_id <- train_activities
-
+# merge training data with test data
 data <- rbind(test_data,train_data)
-
+# name the datset with the activity lablels
 activities <- read.table("activity_labels.txt")
 names(activities) <- c("activity_id","activity_label")
 data <- merge(data,activities,by="activity_id")
-
-colMeans(data[,2:561])
+# Keep only mean and standard deviation variables
+data <- data[,grepl("activity",names(data)) |
+               grepl("subject",names(data)) | 
+               grepl("mean",names(data)) & 
+               !grepl("Freq",names(data)) | 
+               grepl("std",names(data))
+             ]
+data_by_subject <- split(data,data$subject_id)
+TidyData <- as.data.frame(matrix(nrow=0,ncol=67))
+subject_ids <- unique(data$subject_id)
+for (s_id in subject_ids) {
+    TidyData <- rbind(TidyData,cbind(subject_id = s_id,daply(subset(data,subject_id == s_id),.(activity_label),function(y) colMeans(y[,2:67]))))
+}
+TidyData <- cbind(activity = gsub("[0-9]","",rownames(TidyData)),TidyData)
+TidyData <- TidyData[order(as.integer(as.character(TidyData$subject_id))),]
